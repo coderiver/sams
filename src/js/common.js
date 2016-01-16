5ffa665693cb6ed80c5f6fe87d071a2f9a871e0e
akella <akella.a@gmail.com>
2016-01-16 15:48:55 +0200

    samsung

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
-	// go js
+	$('.superslider__slides').slick({
+	  infinite: true,
+	  centerMode: true,
+	  slidesToScroll: 1,
+	  variableWidth: true,
+	  prevArrow: $('.superslider__prev'),
+      nextArrow: $('.superslider__next'),
+	});
+	$('.superslider').on('click', '.superslider__slide', function (e) {
+	  e.stopPropagation();
+	  var index = $(this).data("slick-index");
+	  if ($('.superslider__slides').slick('slickCurrentSlide') !== index) {
+	    $('.superslider__slides').slick('slickGoTo', index);
+	  }
+	});
+
 
 });
